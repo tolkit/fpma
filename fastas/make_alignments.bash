ca914d0de7a0d8c9641ca07a062f5612ba13b811
Max Brown <mb39@sanger.ac.uk>
2022-03-21 17:38:02 +0000

    Add all land plant HMMs

--- fastas/make_alignments.bash
+++ fastas/make_alignments.bash
@@ -2,10 +2,27 @@
 
 # I am using MAFFT version v7.471 (2020/Jul/3).
 
-mkdir fna_alignments
+mkdir angiosperm_fna_alignments
 
-for fasta in ./fastas/*.fasta; do
-    # this is a bit janky
-    nm=$(echo $fasta | cut -d. -f2 | cut -d/ -f3)
-    /Users/mb39/homebrew/Cellar/mafft/7.471/bin/mafft $fasta > ./fna_alignments/${nm}.aln.fasta
+# the taxids of all major land plants
+ANGIOSPERM_TAXID=3398
+GYMNOSPERM_TAXID=1437180
+FERN_TAXID=241806
+LYCOPOD_TAXID=1521260
+HORNWORT_TAXID=13809
+MOSSES_TAXID=3208
+LIVERWORT_TAXID=3195
+
+# make all the directories
+for taxid in {$ANGIOSPERM_TAXID,$GYMNOSPERM_TAXID,$FERN_TAXID,$LYCOPOD_TAXID,$HORNWORT_TAXID,$MOSSES_TAXID,$LIVERWORT_TAXID}; do
+    mkdir "${taxid}_fna_alignments"
+done
+
+for taxid in {$ANGIOSPERM_TAXID,$GYMNOSPERM_TAXID,$FERN_TAXID,$LYCOPOD_TAXID,$HORNWORT_TAXID,$MOSSES_TAXID,$LIVERWORT_TAXID}; do
+    for fasta in ./${taxid}/*.fasta; do
+        # this is a bit janky
+        nm=$(echo $fasta | cut -d. -f2 | cut -d/ -f3)
+        echo $taxid $nm
+        /Users/mb39/homebrew/Cellar/mafft/7.471/bin/mafft $fasta > ./${taxid}_fna_alignments/${nm}.aln.fasta
+    done
 done
--- fastas/make_nuc_alignments.bash
+++ fastas/make_nuc_alignments.bash
@@ -3,10 +3,24 @@
 # aa2nucaln needs to be in path
 # see https://github.com/tolkit/aa2nucaln
 
-mkdir nuc_alignments
+# the taxids of all major land plants
+ANGIOSPERM_TAXID=3398
+GYMNOSPERM_TAXID=1437180
+FERN_TAXID=241806
+LYCOPOD_TAXID=1521260
+HORNWORT_TAXID=13809
+MOSSES_TAXID=3208
+LIVERWORT_TAXID=3195
 
-for aln in ./fna_alignments/*.fasta; do
-    nm=$(echo $aln | cut -d. -f2 | cut -d/ -f3)
-    echo "Processing $nm"
-    aa2nucaln --fasta $aln > ./nuc_alignments/${nm}.fna
+# make all the directories
+for taxid in {$ANGIOSPERM_TAXID,$GYMNOSPERM_TAXID,$FERN_TAXID,$LYCOPOD_TAXID,$HORNWORT_TAXID,$MOSSES_TAXID,$LIVERWORT_TAXID}; do
+    mkdir "${taxid}_nuc_alignments"
+done
+
+for taxid in {$ANGIOSPERM_TAXID,$GYMNOSPERM_TAXID,$FERN_TAXID,$LYCOPOD_TAXID,$HORNWORT_TAXID,$MOSSES_TAXID,$LIVERWORT_TAXID}; do
+    for aln in ./${taxid}_fna_alignments/*.fasta; do
+        nm=$(echo $aln | cut -d. -f2 | cut -d/ -f3)
+        echo "Processing $nm in $aln"
+        aa2nucaln --fasta $aln > ./${taxid}_nuc_alignments/${nm}.fna
+    done
 done
