23925d511fa66345fe6fd92ac9f88f0585c305b4
Max Brown <mb39@sanger.ac.uk>
2022-03-24 21:54:42 +0000

    Update docs.

---
+++ docs/fpma/gff/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["write_gff3","Write a GFF3 to file."]],"static":[["HEADER","GFF3 File Format header."]],"struct":[["GFF3","Vector representation of a GFF3."],["GFF3Field","Fields of the GFF3."]]});
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["fpma"] = {"name":"","files":["cli.rs","lib.rs","plot.rs"]};
+sourcesIndex["fpma"] = {"name":"","files":["cli.rs","gff.rs","lib.rs","plot.rs"]};
 sourcesIndex["pico_args"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
